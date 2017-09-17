5b43d6e5b16fe92823d35e93d6cb43b0de596de9
Serge Assaad <sergeassaad@Serges-MacBook-Pro-2.local>
2017-09-17 19:48:09 -0400

    put data files in folder, wrote test_ReadCSV initial function

---
+++ test_ReadCSV.py
@@ -0,0 +1,16 @@
+"""test_ReadCSV.py
+"""
+
+import sys
+import os
+import pytest
+
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(myPath, '/../mesh/'))
+
+
+def test_ReadCSV():
+	assert ReadCSV('ECG_dummy_data_1.csv') == [[1,2,3], [0.1, 0.4, 0.6]]
+	assert ReadCSV('ECG_dummy_data_2.csv') == [[0.1,0.2,0.3], [0.3, 0.3, 60]]
+	assert ReadCSV('ECG_dummy_data_badformat.csv') == 'This file contains strings'
+	assert ReadCSV('ECG_dummy_data_empty.csv') == 'This file is empty'
