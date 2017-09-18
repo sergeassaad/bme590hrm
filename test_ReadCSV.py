0b686484c60e691b1d39dda099567ebe95e4bc56
Serge Assaad <sergeassaad@Serges-MacBook-Pro-2.local>
2017-09-18 00:54:12 -0400

    removed some comments. Tests finished, closes #2

--- test_ReadCSV.py
+++ test_ReadCSV.py
@@ -1,12 +1,11 @@
 """test_ReadCSV.py
 """
+
 import sys
 import os
 import csv
 import pytest
 from ReadCSV import readCSV
-# myPath = os.path.dirname(os.path.abspath(__file__))
-# sys.path.insert(0, os.path.join(myPath, '/../mesh/'))
 
 def test_ReadCSV():
 	assert readCSV('ECG_dummy_data_1.csv') == [[1,2,3], [0.1, 0.4, 0.6]]
