8cf21ef2b33ca016dd0c45e4a98d4856040df485
Serge Assaad <sergeassaad@Serges-MacBook-Pro-2.local>
2017-09-18 18:17:19 -0400

    test_avgHR tester function, closes #6

---
+++ test_avgHR.py
@@ -0,0 +1,9 @@
+import sys
+import os
+import csv
+import pytest
+
+def test_avgHR():
+	assert test_avgHR(times1, voltages1) = 10
+	assert test_avgHR(times2, voltages2) = 10
+	assert test_avgHR(times3, voltages3) = 10
