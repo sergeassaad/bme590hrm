6bce51b2ea2abb364c5fc73690a53903f5dfe962
Serge Assaad <sergeassaad@Serges-MacBook-Pro-2.local>
2017-09-18 18:18:03 -0400

    minor changes to tester

--- test_avgHR.py
+++ test_avgHR.py
@@ -2,8 +2,26 @@ import sys
 import os
 import csv
 import pytest
+from ReadCSV import readCSV
+from AvgHR import avgHR
 
 def test_avgHR():
-	assert test_avgHR(times1, voltages1) = 10
-	assert test_avgHR(times2, voltages2) = 10
-	assert test_avgHR(times3, voltages3) = 10
+
+	HRlist = [48, 96, 144]
+
+	list1 = readCSV('ECG_dummy_data_1_avgHR.mat')
+	times1 = list1[0]
+	voltages1 = list1[1]
+
+	list2 = readCSV('ECG_dummy_data_2_avgHR.mat')
+	times2 = list2[0]
+	voltages2 = list2[1]
+
+	list3 = readCSV('ECG_dummy_data_3_avgHR.mat')
+	times3 = list3[0]
+	voltages3 = list3[1]
+
+
+	assert avgHR(times1, voltages1) == HRlist[0]
+	assert avgHR(times2, voltages2) == HRlist[1]
+	assert avgHR(times3, voltages3) == HRlist[2]
