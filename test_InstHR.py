aea8d08dcb2140040568cbd2351babcd366a1467
Joseph Cobb <josephcobb@Josephs-MacBook-Pro.local>
2017-09-19 16:21:31 -0400

    Finished unit test for InstHR function. Closes #4

--- test_InstHR.py
+++ test_InstHR.py
@@ -7,16 +7,16 @@ from InstHR import ihr
 
 f = open('dummyEKGdata.csv', 'rU')
 pointer = csv.reader(f)
-exampledata = list(pointer)
+exampleData = list(pointer)
 t = []
 v = []
-for i in range(1,len(exampledata)):
-    t.append(exampledata[i][0])
-    v.append(exampledata[i][1])
+for i in range(1,len(exampleData)):
+    t.append(exampleData[i][0])
+    v.append(exampleData[i][1])
 
 
 def test_inputs():
-    
+
     assert ihr('aaa', [1, 2, 3]) == 'Please input a list of times and a list of lead voltages as arguments.'
     assert ihr([1], [1, 2, 3]) == 'Please input a lists with identical lengths.'
 
