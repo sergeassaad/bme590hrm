95c326553f5b2c3c5f9571080ce01ae255294dca
Hala <hala.el.nahal@duke.edu>
2017-09-19 15:01:35 -0400

    DetectCardia function and edits to unit test

---
+++ Cardia.py
@@ -0,0 +1,12 @@
+def DetectCardia(instHR, time):
+    diagnosis = []
+    time = ([],[],[])
+    for x in instHR:
+        if x<60:
+            diagnosis.append('Bradycardia')
+        elif x>100:
+            diagnosis.append('Tachycardia')
+        elif x>=60 & x<=100:
+            diagnosis.append('Normal')
+
+    return diagnosis
--- test_DetectCardia.py
+++ test_DetectCardia.py
@@ -1,6 +1,8 @@
-
+import Cardia
 
 def test_DetectCardia():
-    (diagnosis,time) = DetectCardia([90,50,110],([0,0.00005],[0.00085,0.0009],[0.0011,0.00115]))
-    assert diagnosis == ['Normal', 'Bradycardia','tachycardia']
-    assert time == ([90,50,110],([0,0.00005],[0.00085,0.0009],[0.0011,0.00115]))
+    diagnosis = Cardia.DetectCardia([90,50,110])
+    assert diagnosis == ['Normal', 'Bradycardia','Tachycardia']
+
+
+
