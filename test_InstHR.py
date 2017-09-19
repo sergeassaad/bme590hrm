517db7f99900906fcec90aa3b54e1dfd9685fa60
Joseph Cobb <josephcobb@Josephs-MacBook-Pro.local>
2017-09-19 16:15:32 -0400

    Finished unit test for InstHR function. Closes #4

--- test_InstHR.py
+++ test_InstHR.py
@@ -16,12 +16,13 @@ for i in range(1,len(exampledata)):
 
 
 def test_inputs():
-
+    
     assert ihr('aaa', [1, 2, 3]) == 'Please input a list of times and a list of lead voltages as arguments.'
     assert ihr([1], [1, 2, 3]) == 'Please input a lists with identical lengths.'
 
 
 def hr_calc():
 
-    assert ihr(t,v) == 0
+    assert ihr(t, v)[0] == [(0.173, 0.517), (0.517, 0.923), (0.923, 1.267), (1.267, 1.673), (1.673, 2.017)]
+    assert ihr(t, v)[1] == [174, 148, 174, 148, 174]
 
