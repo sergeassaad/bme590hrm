7bccc1598652de8d62155e10a7f236abe0575d71
Joseph Cobb <josephcobb@Josephs-MacBook-Pro.local>
2017-09-21 15:39:59 -0400

    Updated unit test. Had to change assert statements (they were incorrect)

--- test_InstHR.py
+++ test_InstHR.py
@@ -11,18 +11,18 @@ exampleData = list(pointer)
 t = []
 v = []
 for i in range(1,len(exampleData)):
-    t.append(exampleData[i][0])
-    v.append(exampleData[i][1])
+    t.append(float(exampleData[i][0]))
+    v.append(float(exampleData[i][1]))
 
 
 def test_inputs():
 
     assert ihr('aaa', [1, 2, 3]) == 'Please input a list of times and a list of lead voltages as arguments.'
-    assert ihr([1], [1, 2, 3]) == 'Please input a lists with identical lengths.'
+    assert ihr([1], [1, 2, 3]) == 'Please input lists with identical lengths.'
 
 
-def hr_calc():
+def test_hr_calc():
 
-    assert ihr(t, v)[0] == [(0.173, 0.517), (0.517, 0.923), (0.923, 1.267), (1.267, 1.673), (1.673, 2.017)]
-    assert ihr(t, v)[1] == [174, 148, 174, 148, 174]
-
+    assert ihr(t, v)[0] == [(0.173, 0.517), (0.517, 0.923), (0.923, 1.267), (1.267, 1.673), (1.673, 2.017),
+                            (2.017, 2.423), (2.423, 2.767)]
+    assert ihr(t, v)[1] == [174.0, 148.0, 174.0, 148.0, 174.0, 148.0, 174.0]
