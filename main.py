638144184109f4d7cda8d35be5e5e0ddf3218738
Hala <hala.el.nahal@duke.edu>
2017-09-24 17:34:32 -0400

    detect_cardia edits in main

--- main.py
+++ main.py
@@ -7,15 +7,18 @@ Function:
 from ReadCSV import readcsv
 from InstHR import ihr
 from AvgHR import avghr
+from Cardia import detect_cardia
 
 def main():
     time_bound1 = 0
     time_bound2 = 10
-    combo(time_bound1, time_bound2)
+    time_ranges='timeRanges'
+    diagnosis_time_threshold=0.001
+    combo(time_bound1, time_bound2,time_ranges, diagnosis_time_threshold )
 
 
 
-def combo(time_bound1, time_bound2):
+def combo(time_bound1, time_bound2, time_ranges, diagnosis_time_threshold):
 
 
     data = readcsv(ecg_data.csv)
@@ -25,8 +28,9 @@ def combo(time_bound1, time_bound2):
     time_pairs = data_2[0]
     heart_rates = data_2[1]
     avghr_value = avghr(times,voltages,time_bound1, time_bound2)
-    data_3 = detectcardia(time_pairs, heart_rates)
-
+    data_3 = detect_cardia(time_pairs, heart_rates, time_ranges, diagnosis_time_threshold)
+    timesranges = data_3[0]
+    diagnosis = data_3[1]
 
 if __name__ == "__main__":
     main()
