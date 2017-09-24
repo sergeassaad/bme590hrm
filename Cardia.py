db112fcb16cdf5141b4ff5488311bfe6a9b48077
Hala <hala.el.nahal@duke.edu>
2017-09-24 17:08:17 -0400

    PEP 8 Compliant closes #9 closes #10

--- Cardia.py
+++ Cardia.py
@@ -1,7 +1,7 @@
-def DetectCardia(instHR, times, timeRanges, diagnosisTimeThreshold):
+def detect_cardia(inst_hr, times, time_ranges, diagnosis_time_threshold):
 
     diagnosis = []
-    for x in instHR:
+    for x in inst_hr:
         if x < 60:
             diagnosis.append("Bradycardia")
         elif x > 100:
@@ -9,75 +9,84 @@ def DetectCardia(instHR, times, timeRanges, diagnosisTimeThreshold):
         elif x >= 60 & x <= 100:
             diagnosis.append("Normal")
 
-    indexN = [i for i, x in enumerate(diagnosis) if x == "Normal"]
-    indexT = [i for i, x in enumerate(diagnosis) if x == "Tachycardia"]
-    indexB = [i for i, x in enumerate(diagnosis) if x == "Bradycardia"]
+    index_n = [i for i, x in enumerate(diagnosis) if x == "Normal"]
+    index_t = [i for i, x in enumerate(diagnosis) if x == "Tachycardia"]
+    index_b = [i for i, x in enumerate(diagnosis) if x == "Bradycardia"]
 
-    nTimes1 = []
-    tTimes1 = []
-    bTimes1 = []
+    n_times = []
+    t_times = []
+    b_times = []
 
-    for n in indexN:
-        nTimes1.append(times[n])
-    for n in indexT:
-        tTimes1.append(times[n])
-    for n in indexB:
-        bTimes1.append(times[n])
+    for n in index_n:
+        n_times.append(times[n])
+    for n in index_t:
+        t_times.append(times[n])
+    for n in index_b:
+        b_times.append(times[n])
 
     timestep = times[0][1]-times[0][0]
 
     i = 0
-    if len(indexN) > 1:
-        while i < len(nTimes1)-1:
-            if nTimes1[i + 1][0] - nTimes1[i][1] <= timestep:
-                nTimes1[i+1] = (nTimes1[i][0],nTimes1[i+1][1])
-                del nTimes1[i]
-            elif nTimes1[i + 1][0] - nTimes1[i][1] > timestep:
-                i=i+1
-
-    i = 0
-    if len(indexT) > 1:
-        while i < len(tTimes1) - 1:
-            if tTimes1[i + 1][0] - tTimes1[i][1] <= timestep:
-                tTimes1[i + 1] = (tTimes1[i][0], tTimes1[i + 1][1])
-                del tTimes1[i]
-            elif tTimes1[i + 1][0] - tTimes1[i][1] > timestep:
+    if len(index_n) > 1:
+        while i < len(n_times)-1:
+            if n_times[i + 1][0] - n_times[i][1] <= timestep:
+                n_times[i+1] = (n_times[i][0],n_times[i+1][1])
+                del n_times[i]
+            elif n_times[i + 1][0] - n_times[i][1] > timestep:
                 i = i+1
 
     i = 0
-    if len(indexB) > 1:
-        while i < len(bTimes1) - 1:
-            if bTimes1[i + 1][0] - bTimes1[i][1] <= timestep:
-                bTimes1[i + 1] = (bTimes1[i][0], bTimes1[i + 1][1])
-                del bTimes1[i]
-            elif bTimes1[i + 1][0] - bTimes1[i][1] > timestep:
+    if len(index_t) > 1:
+        while i < len(t_times) - 1:
+            if t_times[i + 1][0] - t_times[i][1] <= timestep:
+                t_times[i + 1] = (t_times[i][0], t_times[i + 1][1])
+                del t_times[i]
+            elif t_times[i + 1][0] - t_times[i][1] > timestep:
                 i = i+1
 
-    if timeRanges == 'timeRanges':
-        timeRange = "Time ranges: Patient had a normal heart rate between {}, " \
-                    "had tachycardia between {} and had bradycardia between {}".format(nTimes1, tTimes1, bTimes1)
-    if timeRanges == 'noTimeRanges':
-        timeRange = ''
+    i = 0
+    if len(index_b) > 1:
+        while i < len(b_times) - 1:
+            if b_times[i + 1][0] - b_times[i][1] <= timestep:
+                b_times[i + 1] = (b_times[i][0], b_times[i + 1][1])
+                del b_times[i]
+            elif b_times[i + 1][0] - b_times[i][1] > timestep:
+                i = i+1
+
+    if time_ranges == 'timeRanges':
+        time_range = "Time ranges: Patient had a normal heart rate between {}, " \
+                    "had tachycardia between {} and had bradycardia between {}".format(n_times, t_times, b_times)
+    elif time_ranges == 'noTimeRanges':
+        time_range = ''
+    else:
+        time_range = "Time ranges: Error: Please input either 'timeRanges' or 'noTimeRanges'"
 
     i = 0
-    for x in tTimes1:
-        if tTimes1[i][1]-tTimes1[i][0] >= diagnosisTimeThreshold:
+    for x in t_times:
+        if t_times[i][1]-t_times[i][0] >= diagnosis_time_threshold:
             diagnosis1 = 'Tachycardia detected'
         else:
-            diagnosis1 = 'No tachycardia detected'
+            diagnosis1 = 'No Tachycardia detected'
 
     i = 0
-    for x in bTimes1:
-        if bTimes1[i][1]-bTimes1[i][0] >= diagnosisTimeThreshold:
-            diagnosis2='Bradycardia detected'
+    for x in b_times:
+        if b_times[i][1]-b_times[i][0] >= diagnosis_time_threshold:
+            diagnosis2 = 'Bradycardia detected'
         else:
-            diagnosis2='No bradycardia detected'
+            diagnosis2 = 'No Bradycardia detected'
 
     diagnosis = "Diagnosis: {}, {}" .format(diagnosis1, diagnosis2)
 
-    return [timeRange, diagnosis]
+    return [time_range, diagnosis]
+
+def test():
+    detect_cardia([110,90, 50, 110, 70, 70,70,40,40,110], [(0.0005,0.00055),(0, 0.00005), (0.0002, 0.00025),
+                                                          (0.0006, 0.00065),(0.00005,0.0001),(0.0001,0.00015),(0.00015,0.0002),
+                                                          (0.00025,0.0003),(0.0003,0.00035),(0.0018,0.00185)],'timeRanges',0.0001)
 
 
 
+if __name__ == "__main__":
+    test()
 
 
