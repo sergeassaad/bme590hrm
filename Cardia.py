97918ee1b9cce98b83330b41814669b4b623c631
Hala <hala.el.nahal@duke.edu>
2017-09-24 12:42:53 -0400

    DetectCardia function and edits to unit test

--- Cardia.py
+++ Cardia.py
@@ -1,15 +1,82 @@
-def DetectCardia(instHR, times):
+def DetectCardia(instHR, times, timeRanges, diagnosisTimeThreshold):
 
     diagnosis = []
     for x in instHR:
-        if x<60:
-            diagnosis.append('Bradycardia')
-        elif x>100:
-            diagnosis.append('Tachycardia')
-        elif x>=60 & x<=100:
-            diagnosis.append('Normal')
+        if x < 60:
+            diagnosis.append("Bradycardia")
+        elif x > 100:
+            diagnosis.append("Tachycardia")
+        elif x >= 60 & x <= 100:
+            diagnosis.append("Normal")
 
-    return [diagnosis,times]
+    indexN = [i for i, x in enumerate(diagnosis) if x == "Normal"]
+    indexT = [i for i, x in enumerate(diagnosis) if x == "Tachycardia"]
+    indexB = [i for i, x in enumerate(diagnosis) if x == "Bradycardia"]
+
+    nTimes1 = []
+    tTimes1 = []
+    bTimes1 = []
+
+    for n in indexN:
+        nTimes1.append(times[n])
+    for n in indexT:
+        tTimes1.append(times[n])
+    for n in indexB:
+        bTimes1.append(times[n])
+
+    timestep = times[0][1]-times[0][0]
+
+    i = 0
+    if len(indexN) > 1:
+        while i < len(nTimes1)-1:
+            if nTimes1[i + 1][0] - nTimes1[i][1] <= timestep:
+                nTimes1[i+1] = (nTimes1[i][0],nTimes1[i+1][1])
+                del nTimes1[i]
+            elif nTimes1[i + 1][0] - nTimes1[i][1] > timestep:
+                i=i+1
+
+    i = 0
+    if len(indexT) > 1:
+        while i < len(tTimes1) - 1:
+            if tTimes1[i + 1][0] - tTimes1[i][1] <= timestep:
+                tTimes1[i + 1] = (tTimes1[i][0], tTimes1[i + 1][1])
+                del tTimes1[i]
+            elif tTimes1[i + 1][0] - tTimes1[i][1] > timestep:
+                i = i+1
+
+    i = 0
+    if len(indexB) > 1:
+        while i < len(bTimes1) - 1:
+            if bTimes1[i + 1][0] - bTimes1[i][1] <= timestep:
+                bTimes1[i + 1] = (bTimes1[i][0], bTimes1[i + 1][1])
+                del bTimes1[i]
+            elif bTimes1[i + 1][0] - bTimes1[i][1] > timestep:
+                i = i+1
+
+    if timeRanges == 'timeRanges':
+        timeRange = "Time ranges: Patient had a normal heart rate between {}, " \
+                    "had tachycardia between {} and had bradycardia between {}".format(nTimes1, tTimes1, bTimes1)
+    if timeRanges == 'noTimeRanges':
+        timeRange = ''
+
+    i = 0
+    for x in tTimes1:
+        if tTimes1[i][1]-tTimes1[i][0] >= diagnosisTimeThreshold:
+            diagnosis1 = 'Tachycardia detected'
+        else:
+            diagnosis1 = 'No tachycardia detected'
+
+    i = 0
+    for x in bTimes1:
+        if bTimes1[i][1]-bTimes1[i][0] >= diagnosisTimeThreshold:
+            diagnosis2 = 'Bradycardia detected'
+        else:
+            diagnosis2 = 'No bradycardia detected'
+
+    diagnosis = "Diagnosis: {}, {}" .format(diagnosis1, diagnosis2)
+
+    return [timeRange, diagnosis]
+
 
 
 
--- test_DetectCardia.py
+++ test_DetectCardia.py
@@ -1,7 +1,32 @@
 import Cardia
 
 def test_DetectCardia():
-    [diagnosis, times] = Cardia.DetectCardia([90,50,110], ([0,0.00005],[0.0002,0.00025],[0.0006,0.00065]))
-    assert [diagnosis,times] == [['Normal', 'Bradycardia','Tachycardia'],([0,0.00005],[0.0002,0.00025],[0.0006,0.00065])]
+    [timeRange, diagnosis] = Cardia.DetectCardia([110, 90, 50, 110, 70, 70, 70, 40, 40, 110],
+                                                 [(0.0005, 0.00055), (0, 0.00005), (0.0002, 0.00025),
+                                                  (0.0006, 0.00065), (0.00005, 0.0001), (0.0001, 0.00015),
+                                                  (0.00015, 0.0002),
+                                                  (0.00025, 0.0003), (0.0003, 0.00035), (0.0018, 0.00185)],
+                                                 'noTimeRanges', 0.0001)
+    assert [timeRange, diagnosis] == ['', "Diagnosis: Tachycardia detected, Bradycardia detected"]
 
+    [timeRange, diagnosis] = Cardia.DetectCardia([110, 90, 50, 110, 70, 70, 70, 40, 40, 110],
+                                                 [(0.0005, 0.00055), (0, 0.00005), (0.0002, 0.00025),
+                                                  (0.0006, 0.00065), (0.00005, 0.0001), (0.0001, 0.00015),
+                                                  (0.00015, 0.0002),
+                                                  (0.00025, 0.0003), (0.0003, 0.00035), (0.0018, 0.00185)],
+                                                 'timeRanges', 0.0001)
+    assert [timeRange, diagnosis] == ["Time ranges: Patient had a normal heart rate between [(0, 0.0002)], "
+                                      "had tachycardia between [(0.0005, 0.00065), (0.0018, 0.00185)] and had "
+                                      "bradycardia between [(0.0002, 0.00035)]",
+                                      "Diagnosis: Tachycardia detected, Bradycardia detected"]
 
+    [timeRange, diagnosis] = Cardia.DetectCardia([110, 90, 50, 110, 70, 70, 70, 40, 40, 110],
+                                                 [(0.0005, 0.00055), (0, 0.00005), (0.0002, 0.00025),
+                                                  (0.0006, 0.00065), (0.00005, 0.0001), (0.0001, 0.00015),
+                                                  (0.00015, 0.0002),
+                                                  (0.00025, 0.0003), (0.0003, 0.00035), (0.0018, 0.00185)],
+                                                 'timeRanges', 0.1)
+    assert [timeRange, diagnosis] == ["Time ranges: Patient had a normal heart rate between [(0, 0.0002)], "
+                                      "had tachycardia between [(0.0005, 0.00065), (0.0018, 0.00185)] and had "
+                                      "bradycardia between [(0.0002, 0.00035)]",
+                                      "Diagnosis: No tachycardia detected, No bradycardia detected"]
