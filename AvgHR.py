102da20fc78f5f410ef06c47ff28de7b9f9efba5
Serge Assaad <sergeassaad@Serges-MacBook-Pro-2.local>
2017-09-24 16:59:47 -0400

    PEP8 compliance for avghr

--- AvgHR.py
+++ AvgHR.py
@@ -1,48 +1,36 @@
 
-import sys
-import os
-import pytest
+def avghr_unbound(times, voltages):
+    dt = times[1] - times[0]  # assume constant sampling rate
+    threshold_constant = 0.7
+    threshold = threshold_constant * (max(voltages) - min(voltages)) + min(voltages)
+    peak_count = 0
+    peak_times = []
 
-def avgHR_unbound(times, voltages):
+    for i in range(0, len(voltages) - 1):
+        if (voltages[i] >= voltages[i - 1] and voltages[i] > voltages[i + 1]) and voltages[i] > threshold:
+            peak_count += 1
+            peak_times.append(times[i])
 
-	dt = times[1] - times[0] # assume constant sampling rate
-	threshold_constant = 0.7
-	threshold = threshold_constant*(max(voltages)-min(voltages)) + min(voltages)
-	#print(threshold)
-	peak_count = 0
-	peak_times = []
-
-	for i in range(0,len(voltages)-1):
-		if (voltages[i]>=voltages[i-1] and voltages[i]>voltages[i+1]) and voltages[i]>threshold:
-			peak_count += 1
-			peak_times.append(times[i])
-
-	return 60.0*peak_count/(peak_times[-1]-peak_times[0])
-
-def avgHR(times,voltages, t1 = 0, t2 = float('inf')):
-
-	if t1<times[0]:
-		t1 = times[0]
-	if t2>times[len(times)-1]:
-		t2 = times[len(times)-1]
-	if t1>t2:
-		t1_temp = t1
-		t1 = t2
-		t2 = t1_temp
-
-	times_t1 = [abs(t - t1) for t in times]
-	times_t2 = [abs(t - t2) for t in times]
-
-	idx_t1 = times_t1.index(min(times_t1))
-	idx_t2 = times_t2.index(min(times_t2))
-
-	times_bound = times[idx_t1:idx_t2+1]
-	voltages_bound = voltages[idx_t1:idx_t2+1]
-
-	return avgHR_unbound(times_bound, voltages_bound)
+    return 60.0 * peak_count / (peak_times[-1] - peak_times[0])
 
 
+def avghr(times, voltages, t1=0, t2=float('inf')):
+    if t1 < times[0]:
+        t1 = times[0]
+    if t2 > times[len(times) - 1]:
+        t2 = times[len(times) - 1]
+    if t1 > t2:
+        t1_temp = t1
+        t1 = t2
+        t2 = t1_temp
 
+    times_t1 = [abs(t - t1) for t in times]
+    times_t2 = [abs(t - t2) for t in times]
 
+    idx_t1 = times_t1.index(min(times_t1))
+    idx_t2 = times_t2.index(min(times_t2))
 
+    times_bound = times[idx_t1:idx_t2 + 1]
+    voltages_bound = voltages[idx_t1:idx_t2 + 1]
 
+    return avghr_unbound(times_bound, voltages_bound)
--- test_avgHR.py
+++ test_avgHR.py
@@ -1,7 +1,7 @@
 
 
 from ReadCSV import readCSV
-from AvgHR import avgHR
+from AvgHR import avghr
 
 
 def test_avghr():
@@ -20,13 +20,13 @@ def test_avghr():
     times3 = list3[0]
     voltages3 = list3[1]
 
-    assert abs(avgHR(times1, voltages1) - hrlist[0])<5
-    assert abs(avgHR(times2, voltages2) - hrlist[1])<5
-    assert abs(avgHR(times3, voltages3) - hrlist[2])<5
+    assert abs(avghr(times1, voltages1) - hrlist[0])<5
+    assert abs(avghr(times2, voltages2) - hrlist[1])<5
+    assert abs(avghr(times3, voltages3) - hrlist[2])<5
 
     lower_bound1 = times1[0]+0.5*(times1[len(times1)-1]-times1[0])
     lower_bound2 = times2[0]+0.5*(times2[len(times2)-1]-times2[0])
     lower_bound3 = times3[0]+0.5*(times3[len(times3)-1]-times3[0])
-    assert abs(avgHR(times1, voltages1,   lower_bound1,  times1[len(times1)-1]) - hrlist[0]) < 5
-    assert abs(avgHR(times2, voltages2,   lower_bound2,  times2[len(times2)-1]) - hrlist[1]) < 5
-    assert abs(avgHR(times3, voltages3,   lower_bound3,  times3[len(times3)-1]) - hrlist[2]) < 5
+    assert abs(avghr(times1, voltages1,   lower_bound1,  times1[len(times1)-1]) - hrlist[0]) < 5
+    assert abs(avghr(times2, voltages2,   lower_bound2,  times2[len(times2)-1]) - hrlist[1]) < 5
+    assert abs(avghr(times3, voltages3,   lower_bound3,  times3[len(times3)-1]) - hrlist[2]) < 5
