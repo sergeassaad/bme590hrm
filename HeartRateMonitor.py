8d0760fb54ce7fefbd83c011d651ae57869d4185
Serge Assaad <sergeassaad@Serges-MacBook-Pro-2.local>
2017-10-08 15:37:35 -0400

    added avgHR functionality to HRM class. Closes #43

--- HeartRateMonitor.py
+++ HeartRateMonitor.py
@@ -1,4 +1,4 @@
-class HeartRateMonitor:
+class HRM:
     avghr = -1
     insthr = []
     times_insthr = []
@@ -8,3 +8,10 @@ class HeartRateMonitor:
     def __init__(self, times=[], voltages=[], time_units='s'):
         self.times = times
         self.voltages = voltages
+
+    def avghr(self, t1=-1, t2=-1):
+        from AvgHR import *
+        if t1 == -1 or t2== -1:
+            t1 = self.times[0]
+            t2 = self.times[len(self.times)-1]
+        return avghr(self.times, self.voltages, t1, t2)
