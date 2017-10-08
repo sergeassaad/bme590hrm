e387da74797b41b82668a341cf0feb346a2e3d36
Serge Assaad <sergeassaad@Serges-MacBook-Pro-2.local>
2017-10-08 14:42:45 -0400

    PEP8 issues w HRM class

--- HeartRateMonitor.py
+++ HeartRateMonitor.py
@@ -1,12 +1,10 @@
 class HeartRateMonitor:
-	
-	avghr = -1
-	insthr = []
-	times_insthr = []
-	diagnosis = ''
-	times_diagnosis = []
+    avghr = -1
+    insthr = []
+    times_insthr = []
+    diagnosis = ''
+    times_diagnosis = []
 
-	def __init__(self, times = [] , voltages = [] , time_units = 's'):
-	        self.times = times
-	        self.voltages = voltages
-
+    def __init__(self, times=[], voltages=[], time_units='s'):
+        self.times = times
+        self.voltages = voltages
