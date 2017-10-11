b0c11393a92d2626165d7cd73770bfa82f0941a3
Hala <hala.el.nahal@duke.edu>
2017-10-11 08:44:33 -0400

    time_ranges function and unit test

--- Cardia.py
+++ Cardia.py
@@ -1,3 +1,6 @@
+from time_ranges import time_ranges
+
+
 def detect_cardia(inst_hr, times, brady_bound, tachy_bound, display_time_ranges=True, diagnosis_time_threshold=1):
 
     """detect_cardia
@@ -22,58 +25,18 @@ def detect_cardia(inst_hr, times, brady_bound, tachy_bound, display_time_ranges=
         -Time ranges for normal, bradycardia and tachycardia instantaneous HRs
         -Overall diagnosis (for example: 'tachycardia detected') based on the threshold set by the user"""
 
-    diagnosis = []
+    diag = []
     for x in inst_hr:
         if x < brady_bound:
-            diagnosis.append("Bradycardia")
+            diag.append("Bradycardia")
         elif x > tachy_bound:
-            diagnosis.append("Tachycardia")
+            diag.append("Tachycardia")
         elif x >= brady_bound & x <= tachy_bound:
-            diagnosis.append("Normal")
+            diag.append("Normal")
 
-    index_n = [i for i, x in enumerate(diagnosis) if x == "Normal"]
-    index_t = [i for i, x in enumerate(diagnosis) if x == "Tachycardia"]
-    index_b = [i for i, x in enumerate(diagnosis) if x == "Bradycardia"]
-
-    n_times = []
-    t_times = []
-    b_times = []
-
-    for n in index_n:
-        n_times.append(times[n])
-    for n in index_t:
-        t_times.append(times[n])
-    for n in index_b:
-        b_times.append(times[n])
-
-    timestep = times[0][1]-times[0][0]
-
-    i = 0
-    if len(index_n) > 1:
-        while i < len(n_times)-1:
-            if n_times[i + 1][0] - n_times[i][1] <= timestep:
-                n_times[i+1] = (n_times[i][0],n_times[i+1][1])
-                del n_times[i]
-            elif n_times[i + 1][0] - n_times[i][1] > timestep:
-                i = i+1
-
-    i = 0
-    if len(index_t) > 1:
-        while i < len(t_times) - 1:
-            if t_times[i + 1][0] - t_times[i][1] <= timestep:
-                t_times[i + 1] = (t_times[i][0], t_times[i + 1][1])
-                del t_times[i]
-            elif t_times[i + 1][0] - t_times[i][1] > timestep:
-                i = i+1
-
-    i = 0
-    if len(index_b) > 1:
-        while i < len(b_times) - 1:
-            if b_times[i + 1][0] - b_times[i][1] <= timestep:
-                b_times[i + 1] = (b_times[i][0], b_times[i + 1][1])
-                del b_times[i]
-            elif b_times[i + 1][0] - b_times[i][1] > timestep:
-                i = i+1
+    n_times = time_ranges(diag, times, "Normal")
+    b_times = time_ranges(diag, times, "Bradycardia")
+    t_times = time_ranges(diag, times, "Tachycardia")
 
     if display_time_ranges:
         time_range = "Time ranges: Patient had a normal heart rate between {}, " \
