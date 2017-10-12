bfb65a86014605caf286ef2d512707e92f6c4101
Joseph Cobb <josephcobb@Josephs-MacBook-Pro.local>
2017-10-12 15:30:43 -0400

    Added user-input filenames functionality, also uploaded test_data files. Closes #38.

--- main.py
+++ main.py
@@ -21,9 +21,10 @@ def main():
 
 
 def combo(time_bound1, time_bound2, display_time_ranges,
-          diagnosis_time_threshold):
+          diagnosis_time_threshold, input_filename='test_data/test_data1.csv',
+          output_filename='output.txt'):
 
-    data = readcsv('ecg_data.csv')
+    data = readcsv(input_filename)
     times = data[0]
     voltages = data[1]
 
@@ -38,7 +39,7 @@ def combo(time_bound1, time_bound2, display_time_ranges,
     time_ranges = data_3[0]
     diagnosis = data_3[1]
 
-    with open('output.txt', 'w') as f:
+    with open(output_filename, 'w') as f:
         f.write('%s \n' % diagnosis)
 
         if display_time_ranges:
