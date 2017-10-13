117decc6bcde3d2dc2a5f0214dacc03bf1b1cef5
Hala <hala.el.nahal@duke.edu>
2017-10-12 20:25:26 -0400

    Added detect_cardia to class

--- HeartRateMonitor.py
+++ HeartRateMonitor.py
@@ -11,7 +11,7 @@ class HRM:
 
     def avghr(self, t1=-1, t2=-1):
         from AvgHR import avghr
-        if t1 == -1 or t2== -1:
+        if t1 == -1 or t2 == -1:
             t1 = self.times[0]
             t2 = self.times[len(self.times)-1]
         return avghr(self.times, self.voltages, t1, t2)
@@ -19,3 +19,7 @@ class HRM:
     def ihr(self):
         from InstHR import ihr
         return ihr(self.times, self.voltages)
+
+    def detect_Cardia(self, inst_hr, times, display_time_ranges=True, diagnosis_time_threshold=1):
+        from Cardia import detect_cardia
+        return detect_cardia(inst_hr, times, display_time_ranges, diagnosis_time_threshold)
