85bbbc1c7b87e991883549d64c3a99d27860b6e8
Serge Assaad <sergeassaad@Serges-MacBook-Pro-2.local>
2017-10-24 16:51:09 -0400

    added averaging_period attribute, made initial tester for avghr_period function. Closes #76

--- HeartRateMonitor.py
+++ HeartRateMonitor.py
@@ -9,7 +9,7 @@ class HRM:
     -   detect_cardia (finds brady- and tachycardia occurrences)
     """
     def __init__(self, times=[], voltages=[], time_units='s', t1=None, t2=None, display_time_ranges=True,
-                 diagnosis_time_threshold=1):
+                 diagnosis_time_threshold=1, averaging_period = 1):
         self.times = times
         self.voltages = voltages
         self.time_units = time_units
@@ -20,6 +20,7 @@ class HRM:
             t2 = self.times[len(self.times)-1]
         self.t1 = t1
         self.t2 = t2
+        self.averaging_period = averaging_period
         self.instant_hr = []
         self.ihr_times = []
         self.display_time_ranges = display_time_ranges
--- tests/test_avgHR.py
+++ tests/test_avgHR.py
@@ -20,3 +20,22 @@ def test_avghr():
         assert (abs(myHRM.avghr() - hrlist[i - 1]) < 5)
         lower_bound = times[0] + 0.5 * (times[len(times) - 1] - times[0])
         assert abs(avghr(times, voltages, lower_bound, times[len(times) - 1]) - hrlist[i-1]) < 5
+
+
+def test_avghr_period():
+    import math
+    averaging_period = 20
+    hrlist_values = [48, 96, 144]
+
+    filename = 'ECG_dummy_data_1_avgHR.csv'
+    mylist = readcsv(filename)
+    times = mylist[0]
+    voltages = mylist[1]
+
+    dt = times[1]-times[0]
+    number_of_samples = int(math.floor(averaging_period/dt))
+
+    hrlist = [hrlist_values[0]] * number_of_samples
+
+    myHRM = HRM(times, voltages, averaging_period = averaging_period)
+    assert myHRM.avghr() == hrlist
