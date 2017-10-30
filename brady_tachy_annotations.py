d1f2d3e79e79014d0e0f2772c4c848e935c1a5a4
Hala <hala.el.nahal@duke.edu>
2017-10-30 06:03:42 -0400

    bradycardia and tachycardia annotations for CloudEGC assignment

---
+++ brady_tachy_annotations.py
@@ -0,0 +1,36 @@
+def cardia_annotations(list, brady_bound, tachy_bound):
+
+    """cardia_annotations
+
+    Module Author: Hala El-Nahal
+
+    Function: Outputs bradycardia and tachycardia annotation strings: strings of true or false
+
+    Four input arguments:
+        -arg 1: any list, instant or average HR
+        -arg 2: bound for bradycardia
+        -arg 3: bound for tachycardia
+
+    Two outputs:
+        -brady: bradycardia annotations
+        -tachy: tachycardia annotations
+    """
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    brady = []
+    tachy = []
+    for x in list:
+        if x <= brady_bound:
+            brady.append("true")
+        elif x > brady_bound:
+            brady.append("false")
+        elif x >= tachy_bound:
+            tachy.append("true")
+        elif x < tachy_bound:
+            tachy.append("false")
+
+    return [brady, tachy]
+
+
+
