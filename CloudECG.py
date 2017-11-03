32ad109af2a8c4fff461520af6276f061153388b
Serge Assaad <sergeassaad@Serges-MacBook-Pro-2.local>
2017-11-03 10:55:33 -0400

    added try-except statements to CloudECG.py

--- CloudECG.py
+++ CloudECG.py
@@ -5,12 +5,22 @@ app = Flask(__name__)
 req_num = 0
 
 
+def send_error(message, code):
+    err = {
+        "error": message
+    }
+    return jsonify(err), code
+
 @app.route("/heart_rate/summary", methods=['POST'])
 def cloud_ecg_summary():
     from HeartRateMonitor import HRM
     global req_num
     req_num += 1
-    obj = HRM(request.json['time'], request.json['voltage'])
+    try:
+        obj = HRM(request.json['time'], request.json['voltage'])
+    except ValueError:
+        return send_error("Improper time & voltage arrays", 400)
+
     instant_hr = obj.ihr()
     obj.avghr()
     obj.detect_cardia()
@@ -29,8 +39,11 @@ def cloud_ecg_average():
     from HeartRateMonitor import HRM
     global req_num
     req_num += 1
-    obj = HRM(request.json['time'], request.json['voltage'],
+    try:
+        obj = HRM(request.json['time'], request.json['voltage'],
               averaging_period = request.json['averaging_period'])
+    except ValueError:
+        return send_error("Improper time & voltage arrays, averaging period", 400)
     obj.ihr()
     obj.avghr()
     obj.detect_cardia()
